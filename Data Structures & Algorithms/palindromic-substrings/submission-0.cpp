cca31252a72b7d40120078f8d7a8f8f2a8955c0e
Ashmit Tyagi <151730397+Ashmit-Tyagi@users.noreply.github.com>
2026-07-14 16:11:54 +0530

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int res=0;
+        for(int i=0;i<s.size();i++){
+            res+=countP(s,i,i);
+            res+=countP(s,i,i+1);
+        }
+        return res;
+    }
+    int countP(string s, int l, int r){
+        int res=0;
+        while(l>=0 && r<s.size() && s[l]==s[r]){
+            res++;
+            l--;
+            r++;
+        }
+        return res;
+    }
+};
